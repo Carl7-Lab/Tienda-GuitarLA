0480bba5e67e64939627957d2c154d2fe955d236
Carl7 <carlosmiguel7cb@gmail.com>
2022-05-29 09:27:39 -0500

    se creo la pagina tienda.js

---
+++ pages/tienda.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+function Tienda() {
+  return (
+    <Layout
+      pagina='Tienda'
+    >
+      <h1>Desde Tienda</h1>
+    </Layout>
+  )
+}
+
+export default Tienda
