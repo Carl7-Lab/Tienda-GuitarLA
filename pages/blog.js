4fd347c233fcced36588001d7fe463db18ac3386
Carl7 <carlosmiguel7cb@gmail.com>
2022-05-29 09:27:19 -0500

    se creo la pagina blog.js

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+function Blog() {
+  return (
+    <Layout
+      pagina='Blog'
+    >
+      <h1>Desde Blog</h1>
+    </Layout>
+  )
+}
+
+export default Blog
