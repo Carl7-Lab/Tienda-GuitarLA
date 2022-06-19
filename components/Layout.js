a2842d300ca8567c09be93e6330a4ef9c6b9d5bd
Carl7 <carlosmiguel7cb@gmail.com>
2022-06-19 10:58:09 -0500

    se agrego en la navegacion el link del carrito y al header se agrego la visualizacion de una guitarra con sus respectivos estilos

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
 
-function Layout({children, pagina}) {
+function Layout({children, pagina, guitarra}) {
   return (
     <div>
       <Head>
@@ -10,13 +10,19 @@ function Layout({children, pagina}) {
         <meta name="description" content="Sitio Web de Ventas de Guitarras" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <Header
+        guitarra={guitarra}
+      />
       
       {children}
 
       <Footer />
     </div>
   )
+}
+
+Layout.defaultProps = {
+  guitarra: null
 }
 
 export default Layout
